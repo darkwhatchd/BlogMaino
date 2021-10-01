508d44b35391efb892a2bbd41158ffa0baeeadaf
Daniel Evani <danielevani97@gmail.com>
2021-10-01 15:19:55 -0300

    ActiveStorage configured to Post cover attachment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :resume, :user_id)
+      params.require(:post).permit(:title, :resume, :cover,:user_id)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_many :commentaries
+  has_one_attached :cover do |attachable|
+    attachable.variant :thumb, resize: "300x300"
+  end
 
   validates :title, :cover, :resume, presence: true
 end
