7765880c1f9b5aeceee276cade83c6bd73b73437
Daniel Evani <danielevani97@gmail.com>
2021-10-04 05:00:58 -0300

    Changes to send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   #config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = {:host => 'damp-basin-30375.herokuapp.com'}
+  
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => 'https://damp-basin-30375.herokuapp.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
