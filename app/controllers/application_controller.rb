e675c901b5c0c6a81cdf4751b7f0e4635d1c08fd
Daniel Evani <danielevani97@gmail.com>
2021-10-04 16:07:35 -0300

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+ 
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale].present?
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 
   protected
 
