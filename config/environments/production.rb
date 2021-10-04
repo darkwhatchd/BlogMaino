18addeb2473f99b79dd5fc62a2b4a709db0371e3
Daniel Evani <danielevani97@gmail.com>
2021-10-04 02:15:06 -0300

    Enviroment updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,7 @@ Rails.application.configure do
   #Rails.application.credentials.dig(:sendgrid, :api_key)
   config.action_mailer.default_url_options = {:host => 'https://damp-basin-30375.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    password: Rails.application.credentials.api_key, # This is the secret sendgrid API key which was issued during API key creation
-    domain: 'https://damp-basin-30375.herokuapp.com/',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
