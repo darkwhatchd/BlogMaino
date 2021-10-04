451367c9a97dbb39febc7e5384e68dc5b3883ee4
Daniel Evani <danielevani97@gmail.com>
2021-10-04 05:38:47 -0300

    Changes to send mail

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  password: ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
+  user_name: Rails.application.credentials.dig(:sendgrid, :user_name), # This is the string literal 'apikey', NOT the ID of your API key
+  password: Rails.application.credentials.dig(:sendgrid, :password), # This is the secret sendgrid API key which was issued during API key creation
   domain: 'damp-basin-30375.herokuapp.com',
   address: 'smtp.sendgrid.net',
-  port: 465,
-  authentication: :plain,
+  port: 587,
+  authentication: :login,
   enable_starttls_auto: true
 }
